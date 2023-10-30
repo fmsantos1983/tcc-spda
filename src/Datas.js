8f46636c50b5f3d70cbefa70cb3f6186d8bdabdf
fmsantos1983 <ti.felipe.santos@gmail.com>
2023-10-30 15:37:43 -0300

    30102023

---
+++ src/Datas.js
@@ -0,0 +1,15 @@
+export const parametros_s1_d1 = [
+    {
+        title: "Tabela 1 – Tipo de dano - Componente relacionado a ferimentos a seres vivos - D1",
+        subtitle1: "Parâmetro",
+        subtitle2: "Descrição",
+        subtitle3: "Referência",
+        subtitle4: "Nível 1",
+        subtitle5: "Nível 2",
+        subtitle6: "Nível 3",
+        subtitle7: "Nível 4",
+        subtitle8: "Nível 5",
+
+    }
+
+]
